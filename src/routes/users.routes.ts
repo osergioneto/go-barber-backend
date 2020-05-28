5aa670503013193fe18b54a9330ba6be5dd7cc79
Sérgio Neto <sergio.deusdedith@cubos.io>
2020-05-28 00:38:42 -0300

    feat: add create user service

---
+++ src/routes/users.routes.ts
@@ -0,0 +1,21 @@
+import { Router } from "express";
+import CreateUserService from "../services/CreateUserService";
+
+const usersRouter = Router();
+
+usersRouter.post('/', async (req, res) => {
+  try {
+    const { name, email, password } =  req.body;
+
+    const createUserService = new CreateUserService();
+    const user = createUserService.execute({ name, email, password });
+
+    return res.status(201).json(user);
+  } catch ({ message }) {
+    console.log("user: ", message);
+    return res.status(400).json({ error: message })
+  }
+  
+});
+
+export default usersRouter;
