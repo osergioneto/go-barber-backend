b63e149dd6d86387c5576df2fa9ed15750bd9850
Sérgio Neto <sergio.deusdedith@cubos.io>
2020-05-28 23:26:58 -0300

    feat: encrypt passwords

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -5,6 +5,7 @@ interface Request {
 }
 import User from "../models/User";
 import { getRepository } from "typeorm";
+import { hash } from "bcryptjs";
 
 export default class CreateUserService {
   public async execute({ name, email, password }: Request): Promise<User> {
@@ -18,11 +19,15 @@ export default class CreateUserService {
       throw new Error("Email address already used");
     }
 
+    const hashPassword = await hash(password, 8);
+
     const createdUser = await userRepository.save({
       name,
       email,
-      password
+      password: hashPassword
     });
+
+    delete createdUser.password;
 
     return createdUser;
   }
