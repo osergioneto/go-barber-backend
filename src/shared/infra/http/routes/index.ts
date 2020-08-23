9022aad4590650f6e3dd027a0aebc8dc19ecf9f9
Sérgio Neto <sergio.deusdedith@gmail.com>
2020-08-23 20:38:16 -0300

    feat: add password routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,9 +3,11 @@ const routes = Router();
 import appointmentsRouter from '@modules/appointments/infra/http/routes/appointments.routes';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import authRouter from '@modules/users/infra/http/routes/auth.routes';
+import passwordRouter from '@modules/users/infra/http/routes/password.routes';
 
 routes.use('/auth', authRouter);
 routes.use('/appointments', appointmentsRouter);
 routes.use('/users', usersRouter);
+routes.use('/password', passwordRouter);
 
 export default routes;
