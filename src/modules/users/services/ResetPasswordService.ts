9193e0399a2ac7a48ee08c831eb3187e54b0f5eb
Sérgio Neto <sergio.deusdedith@gmail.com>
2020-08-23 19:22:07 -0300

    feat: add hash to password. validate token expiraton

--- src/modules/users/services/ResetPasswordService.ts
+++ src/modules/users/services/ResetPasswordService.ts
@@ -1,7 +1,10 @@
-import IUsersRepository from '../repositories/IUsersRepositories';
 import { inject, injectable } from 'tsyringe';
+import { isAfter, addHours } from 'date-fns';
+
 import AppError from '@shared/errors/AppError';
+import IUsersRepository from '../repositories/IUsersRepositories';
 import IUserTokensRepository from '../repositories/IUserTokensRepositories';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
 
 interface Request {
   token: string;
@@ -16,6 +19,9 @@ export default class ResetPasswordService {
 
     @inject('UserTokensRepository')
     private usersTokensRepository: IUserTokensRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
   ) {}
 
   public async execute({ token, password }: Request): Promise<void> {
@@ -25,13 +31,20 @@ export default class ResetPasswordService {
       throw new AppError('User token does not exists');
     }
 
-    const user = await this.usersRepository.findById(userToken?.token);
+    const user = await this.usersRepository.findById(userToken.user_id);
 
     if (!user) {
       throw new AppError('User does not exists');
     }
 
-    user.password = password;
+    const tokenCreatedAt = userToken.created_at;
+    const compareDate = addHours(tokenCreatedAt, 2);
+
+    if (isAfter(Date.now(), compareDate)) {
+      throw new AppError('Token expired');
+    }
+
+    user.password = await this.hashProvider.generateHash(password);
 
     await this.usersRepository.save(user);
   }
