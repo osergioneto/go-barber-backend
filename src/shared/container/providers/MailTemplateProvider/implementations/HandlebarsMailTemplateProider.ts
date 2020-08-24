a3e26237f4390871e62a4363a19700f58c97b8c6
Sérgio Neto <sergio.deusdedith@gmail.com>
2020-08-24 00:30:25 -0300

    feat: add a handlebars as template engine

---
+++ src/shared/container/providers/MailTemplateProvider/implementations/HandlebarsMailTemplateProider.ts
@@ -0,0 +1,16 @@
+import IParseMailTemplateDTO from '../dtos/IParseMailTemplateDTO';
+import IMailTemplateProvider from '../models/IMailTemplateProvider';
+import handlebars from 'handlebars';
+
+class HandlebarsMailTemplateProvider implements IMailTemplateProvider {
+  public async parse({
+    template,
+    variables,
+  }: IParseMailTemplateDTO): Promise<string> {
+    const parseTemplate = handlebars.compile(template);
+
+    return parseTemplate(variables);
+  }
+}
+
+export default HandlebarsMailTemplateProvider;
