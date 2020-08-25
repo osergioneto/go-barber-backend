dcd7efa0589e9fb4885227a1ec4230b09cbcf4ce
Sérgio Neto <sergio.deusdedith@gmail.com>
2020-08-25 00:50:17 -0300

    feat: add providers routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -5,11 +5,13 @@ import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import authRouter from '@modules/users/infra/http/routes/auth.routes';
 import passwordRouter from '@modules/users/infra/http/routes/password.routes';
 import profileRouter from '@modules/users/infra/http/routes/profile.routes';
+import providersRouter from '@modules/appointments/infra/http/routes/providers.routes';
 
 routes.use('/auth', authRouter);
 routes.use('/appointments', appointmentsRouter);
 routes.use('/users', usersRouter);
 routes.use('/password', passwordRouter);
 routes.use('/profile', profileRouter);
+routes.use('/providers', providersRouter);
 
 export default routes;
