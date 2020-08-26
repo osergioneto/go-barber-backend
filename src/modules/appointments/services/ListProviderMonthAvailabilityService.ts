c9ce6e7883b0fc4ce38f39201ebeb3a9e2ae0b94
Sérgio Neto <sergio.deusdedith@gmail.com>
2020-08-25 22:36:22 -0300

    feat: add service to list all dates available on month from provider

---
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -0,0 +1,37 @@
+import AppError from '@shared/errors/AppError';
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import { inject, injectable } from 'tsyringe';
+import User from '@modules/users/infra/typeorm/entities/User';
+
+interface Request {
+  provider_id: string;
+  month: number;
+  year: number;
+}
+
+type IResponse = Array<{
+  day: number;
+  available: boolean;
+}>;
+
+@injectable()
+export default class ListProviderMonthAvailabilityService {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
+
+  public async execute({ provider_id, month, year }: Request) {
+    const appointments = await this.appointmentsRepository.findAllInMonthFromProvider(
+      {
+        provider_id,
+        month,
+        year,
+      },
+    );
+
+    console.log('appointments: ', appointments);
+
+    return appointments;
+  }
+}
