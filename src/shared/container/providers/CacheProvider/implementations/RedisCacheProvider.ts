b3c3fd4a675aebb53074e55fcb54798fad4215e5
Sérgio Neto <sergio.deusdedith@gmail.com>
2020-09-03 23:26:07 -0300

    feat: add cache with redis

---
+++ src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
@@ -0,0 +1,22 @@
+import ICacheProvider from '../models/ICacheProvider';
+import Redis, { Redis as RedisClient } from 'ioredis';
+import cacheConfig from '@config/cache';
+import { isValueProvider } from 'tsyringe';
+
+export default class RedisCacheProvider implements ICacheProvider {
+  private client: RedisClient;
+
+  constructor() {
+    this.client = new Redis(cacheConfig.config.redis);
+  }
+
+  public async save(key: string, value: string): Promise<void> {
+    this.client.set(key, value);
+  }
+
+  public async recover(key: string): Promise<string | null> {
+    return this.client.get(key);
+  }
+
+  public async invalidate(key: string): Promise<void> {}
+}
