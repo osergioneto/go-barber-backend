29e7ec35bdaae9c8f59ca8e63be8bdba37b269f1
Sérgio Neto <sergio.deusdedith@gmail.com>
2020-09-03 23:58:48 -0300

    feat: add function to clean multiple keys from cache

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -8,6 +8,7 @@ import AppError from '@shared/errors/AppError';
 import IUsersRepository from '../repositories/IUsersRepositories';
 import { inject, injectable } from 'tsyringe';
 import IHashProvider from '../providers/HashProvider/models/IHashProvider';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 @injectable()
 export default class CreateUserService {
@@ -16,6 +17,8 @@ export default class CreateUserService {
     private usersRepository: IUsersRepository,
     @inject('HashProvider')
     private hashProvider: IHashProvider,
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({ name, email, password }: Request): Promise<User> {
@@ -32,6 +35,8 @@ export default class CreateUserService {
       email,
       password: hashPassword,
     });
+
+    await this.cacheProvider.invalidatePrefix(`providers-list`);
 
     return createdUser;
   }
