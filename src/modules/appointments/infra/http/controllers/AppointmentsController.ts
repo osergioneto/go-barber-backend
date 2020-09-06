7e7ee53352a224fe701acf62f2f562c143fd5112
Sérgio Neto <sergio.deusdedith@gmail.com>
2020-09-05 21:49:31 -0300

    feat: invalidate cache of day when appointment is created

--- src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -7,7 +7,6 @@ export default class AppointmentsController {
   public async create(request: Request, response: Response): Promise<Response> {
     const user_id = request.user.id;
     const { provider_id, date } = request.body;
-    const parsedDate = parseISO(date);
 
     const createAppointmentService = container.resolve(
       CreateAppointmentService,
@@ -16,7 +15,7 @@ export default class AppointmentsController {
     const appointment = await createAppointmentService.execute({
       provider_id,
       user_id,
-      date: parsedDate,
+      date,
     });
 
     return response.status(201).json(appointment);
--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -5,6 +5,7 @@ import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 import AppError from '../../../shared/errors/AppError';
 import { inject, injectable } from 'tsyringe';
 import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 interface IRequest {
   provider_id: string;
   user_id: string;
@@ -19,6 +20,9 @@ class CreateAppointmentService {
 
     @inject('NotificationsRepository')
     private notificationsRepository: INotificationsRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({
@@ -62,6 +66,13 @@ class CreateAppointmentService {
       recipient_id: provider_id,
       content: `Novo agendamento para dia ${dateFormated}`,
     });
+
+    await this.cacheProvider.invalidate(
+      `provider-appointments:${provider_id}:${format(
+        appointmentDate,
+        'yyyy-M-d',
+      )}`,
+    );
 
     return appointment;
   }
