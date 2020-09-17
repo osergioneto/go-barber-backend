809655f23f5a79e12302272b9643a5a44513677c
Sérgio Neto <sergio.deusdedith@cubos.io>
2020-09-16 23:48:13 -0300

    fix: add classToClass on some data

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -3,6 +3,7 @@ import IUsersRepository from '@modules/users/repositories/IUsersRepositories';
 import { inject, injectable } from 'tsyringe';
 import User from '@modules/users/infra/typeorm/entities/User';
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+import { classToClass } from 'class-transformer';
 
 interface Request {
   user_id: string;
@@ -28,7 +29,7 @@ export default class ListProvidersService {
         except_user_id: user_id,
       });
 
-      await this.cacheProvider.save(`providers-list:${user_id}`, users);
+      await this.cacheProvider.save(`providers-list:${user_id}`, classToClass(users));
     }
 
     return users;
